bee6b0af8127c6815dbc88e2e5ef7ffaecb11f19
vijay <kumarvijay54569@gmail.com>
2024-08-07 18:15:47 +0530

    first commit

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -883,10 +883,12 @@ const Portfolio = () => {
           </div>
         </div>
       </div>
-      <div className="our_work_section">
-        <h1 className="our_work_heading">Impacts created</h1>
-        <div className="our_work_content">
-          <Carousel className="carosel"
+      <div className="our_work_section" data-aos="fade-up">
+      <h1 className="our_work_heading" data-aos="fade-right">Impacts created</h1>
+      <div className="our_work_content">
+        <div className="carousel_container" data-aos="fade-left">
+          <Carousel
+            className="carousel"
             showArrows={true}
             showThumbs={false}
             showIndicators={true}
@@ -905,25 +907,12 @@ const Portfolio = () => {
               <img src={MCDMFWebsiteImg3} alt="Another Work 2" className="work_img" />
             </div>
           </Carousel>
-          <ul className="work_description">
-  <li>
-    We have successfully developed and launched the MCDMF website, which showcases our expertise in web development.
-  </li>
-  <li>
-    At MCDMF - Mainframe Services, we believe in providing personalized IT solutions to our clients.
-  </li>
-  <li>
-    We understand that every business is unique, and so are its IT needs.
-  </li>
-  <li>
-    That's why we work closely with our clients to understand their requirements and provide tailored solutions that fit their needs and budget.
-  </li>
-  <li>
-    We also offer flexible pricing models to ensure that our services are accessible to all businesses.
-  </li>
-</ul>
         </div>
+        <p className="work_description" data-aos="fade-up">
+          We have successfully developed and launched the MCDMF website, which showcases our expertise in web development. At MCDMF - Mainframe Services, we believe in providing personalized IT solutions to our clients. We understand that every business is unique, and so are its IT needs. That’s why we work closely with our clients to understand their requirements and provide tailored solutions that fit their needs and budget. Additionally, we offer flexible pricing models to ensure that our services are accessible to all businesses.
+        </p>
       </div>
+    </div>
     </>
   );
 };
